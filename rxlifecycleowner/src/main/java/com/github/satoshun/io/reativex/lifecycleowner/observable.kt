c4fccb440793679eab3dc8cc6ce9c09c9b468615
satoshun <shun.sato1@gmail.com>
2017-08-17 00:53:28 +0000

    consider ViewModel architecture component

--- rxlifecycleowner/src/main/java/com/github/satoshun/io/reativex/lifecycleowner/observable.kt
+++ rxlifecycleowner/src/main/java/com/github/satoshun/io/reativex/lifecycleowner/observable.kt
@@ -1,26 +1,37 @@
 package com.github.satoshun.io.reativex.lifecycleowner
 
 import android.arch.lifecycle.Lifecycle
-import android.arch.lifecycle.LifecycleObserver
 import android.arch.lifecycle.LifecycleOwner
-import android.arch.lifecycle.OnLifecycleEvent
+import android.support.annotation.MainThread
 import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
+import io.reactivex.exceptions.OnErrorNotImplementedException
 
-// todo only support onNext
-fun <T : Any> Observable<T>.subscribeBy(owner: LifecycleOwner, onNext: (T) -> Unit): Disposable {
-  val disposable = subscribe(onNext)
-  owner.lifecycle.addObserver(LifecycleBoundObserver(disposable))
+private val onNextEmpty: (Any) -> Unit = {}
+private val onErrorEmpty: (Throwable) -> Unit = { throw OnErrorNotImplementedException(it) }
+private val onCompleteEmpty: () -> Unit = {}
+
+@MainThread
+fun <T : Any> Observable<T>.subscribeBy(
+    owner: LifecycleOwner,
+    event: Lifecycle.Event = Lifecycle.Event.ON_DESTROY,
+    onNext: (T) -> Unit = onNextEmpty,
+    onError: (Throwable) -> Unit = onErrorEmpty,
+    onComplete: () -> Unit = onCompleteEmpty
+): Disposable {
+  val disposable = subscribe(onNext, onError, onComplete)
+  owner.lifecycle.addObserver(LifecycleBoundObserver(disposable, event))
   return disposable
 }
 
-// todo: now only support ON_DESTROY EVENT
-private class LifecycleBoundObserver(
-    private val disposable: Disposable
-) : LifecycleObserver {
-
-  @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-  fun onStateChange() {
-    disposable.takeIf { !it.isDisposed }?.dispose()
-  }
+@MainThread
+fun <T : Any> Observable<T>.subscribeBy(
+    viewModel: RxViewModel,
+    onNext: (T) -> Unit = onNextEmpty,
+    onError: (Throwable) -> Unit = onErrorEmpty,
+    onComplete: () -> Unit = onCompleteEmpty
+): Disposable {
+  val disposable = subscribe(onNext, onError, onComplete)
+  viewModel.addDisposable(disposable)
+  return disposable
 }
